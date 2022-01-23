2585639d5b402e6627476c18d5f2b457356fcc2b
jackfperryjr <jackfperryjr@gmail.com>
2022-01-23 11:10:44 -0600

    Applied edits from Sarah. Added JavaScript calculation for number of years ago.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 function App() {
+  let years = new Date().getFullYear() - 2002
+
   return (
     <div className="app">
       <header>
@@ -10,11 +12,13 @@ function App() {
       <h1><a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer">States of Joi</a></h1>
       <hr/>
       <div className="content-container">
-        <p>Joi is a character that I started in 1998 with the intention of doing something with it. Well, that never happened.</p>
-        <br/><p>Until recently.</p><br/>
-        <p>I'm a software engineer <i>and</i> an artist. While I don't always stay on top of the lastest technology trends, or fads, I like to read what I can, when I can. The more I dove into non-fungible tokens (<a href="https://en.wikipedia.org/wiki/Non-fungible_token" target="_blank" rel="noreferrer">NFTs</a>), the more curious I became. This was an opportunity to combine two of my passions: technology and art. I had already played around with some cryptocurrencies, so I had a hosted wallet (<a className="primary" href="https://www.coinbase.com" target="_blank" rel="noreferrer">Coinbase</a>). Now, I just needed a marketplace. Well, long story short, I learned that I needed a software wallet (<a href="https://www.coinbase.com/wallet" target="_blank" rel="noreferrer">Coinbase Wallet</a>) for the marketplace (<a className="primary" href="https://opensea.io" target="_blank" rel="noreferrer">OpenSea</a>) I planned to mint and list my NFTs for sale.<sup><small><i>1</i></small></sup></p>
-        <br/><p><strong>Back to Joi!</strong></p><br/>
-        <p>Joi didn't even have a name until I started this project. She was also never intended to have a face. You see, Joi is from another planet and has the ability to change the appearance of her face, hair, and skin. Over her lifetime she's taken on different states of being when learning different emotions in different environments, or galaxies. The collection is ever evolving. There are 306 unique tokens currently and I have dozens more in the works that haven't been minted yet. So, go <a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer">explore the states of Joi</a> and check back often for more!</p>
+        <p>Joi is a character that I started about {years} years ago with the intention to bring her story to life in a cool way.</p>
+        <br/><p>Decades later, I've found a way to share Joi's story.</p><br/>
+        <p>I'm a software engineer <i>and</i> an artist. I like to stay up-to-date on technology trends and fads and dabble here and there. The more I dove into non-fungible tokens (<a href="https://en.wikipedia.org/wiki/Non-fungible_token" target="_blank" rel="noreferrer">NFTs</a>), the more curious I became. This was an opportunity to combine two of my passions: technology and art. I already had a hosted wallet (<a className="primary" href="https://www.coinbase.com" target="_blank" rel="noreferrer">Coinbase</a>) to play around with some cryptocurrencies. Next, I selected a marketplace (<a className="primary" href="https://opensea.io" target="_blank" rel="noreferrer">OpenSea</a>) and setup a software wallet (<a href="https://www.coinbase.com/wallet" target="_blank" rel="noreferrer">Coinbase Wallet</a>). Now I am minting and listing my NFTs for sale.<sup><small><i>1</i></small></sup></p>
+        <br/>
+        <p>Joi didn't have a name until I started this project. The concept for her character began with her not having a face. You see, Joi is from another planet and has the ability to change the appearance of her face, hair, and skin. Over her lifetime, she has taken on different states of being as her travels expose her to different emotions in different environments, even in different galaxies.</p>
+        <br/>
+        <p>Joi's collection is ever-evolving. Currently there are 306 unique tokens, and I have dozens more in the works that are not yet minted. So, go <a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer">explore the states of Joi</a> and check back often for more!</p>
         <br/>
         <p>You can also follow the project on <a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a> <a href="https://twitter.com/statesofjoi" target="_blank" rel="noreferrer">@statesofjoi</a>.</p>
       </div>
