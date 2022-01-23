1fcb956c454f9dbda524d78b9e785cc80c3a041d
jackfperryjr <jackfperryjr@gmail.com>
2022-01-23 10:34:47 -0600

    Adjusted for warnings/errors.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ function App() {
       <header>
         <div className="header-container"></div>
       </header>
-      <a href="https://opensea.io/collection/states-of-joi" target="_blank"><div className="logo-container"></div></a>
-      <h1><a href="https://opensea.io/collection/states-of-joi" target="_blank">States of Joi</a></h1>
+      <a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer"><div className="logo-container"></div></a>
+      <h1><a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer">States of Joi</a></h1>
       <hr/>
       <div className="content-container">
         <p>Joi is a character that I started in 1998 with the intention of doing something with it. Well, that never happened.</p>
         <br/><p>Until recently.</p><br/>
-        <p>I'm a software engineer <i>and</i> an artist. While I don't always stay on top of the lastest technology trends, or fads, I like to read what I can, when I can. The more I dove into non-fungible tokens (<a href="https://en.wikipedia.org/wiki/Non-fungible_token" target="_blank">NFTs</a>), the more curious I became. This was an opportunity to combine two of my passions: technology and art. I had already played around with some cryptocurrencies, so I had a hosted wallet (<a className="primary" href="https://www.coinbase.com" target="_blank">Coinbase</a>). Now, I just needed a marketplace. Well, long story short, I learned that I needed a software wallet (<a href="https://www.coinbase.com/wallet" target="_blank">Coinbase Wallet</a>) for the marketplace (<a className="primary" href="https://opensea.io" target="_blank">OpenSea</a>) I planned to mint and list my NFTs for sale.<sup><small><i>1</i></small></sup></p>
+        <p>I'm a software engineer <i>and</i> an artist. While I don't always stay on top of the lastest technology trends, or fads, I like to read what I can, when I can. The more I dove into non-fungible tokens (<a href="https://en.wikipedia.org/wiki/Non-fungible_token" target="_blank" rel="noreferrer">NFTs</a>), the more curious I became. This was an opportunity to combine two of my passions: technology and art. I had already played around with some cryptocurrencies, so I had a hosted wallet (<a className="primary" href="https://www.coinbase.com" target="_blank" rel="noreferrer">Coinbase</a>). Now, I just needed a marketplace. Well, long story short, I learned that I needed a software wallet (<a href="https://www.coinbase.com/wallet" target="_blank" rel="noreferrer">Coinbase Wallet</a>) for the marketplace (<a className="primary" href="https://opensea.io" target="_blank" rel="noreferrer">OpenSea</a>) I planned to mint and list my NFTs for sale.<sup><small><i>1</i></small></sup></p>
         <br/><p><strong>Back to Joi!</strong></p><br/>
-        <p>Joi didn't even have a name until I started this project. She was also never intended to have a face. You see, Joi is from another planet and has the ability to change the appearance of her face, hair, and skin. Over her lifetime she's taken on different states of being when learning different emotions in different environments, or galaxies. The collection is ever evolving. There are 306 unique tokens currently and I have dozens more in the works that haven't been minted yet. So, go <a href="https://opensea.io/collection/states-of-joi" target="_blank">explore the states of Joi</a> and check back often for more!</p>
+        <p>Joi didn't even have a name until I started this project. She was also never intended to have a face. You see, Joi is from another planet and has the ability to change the appearance of her face, hair, and skin. Over her lifetime she's taken on different states of being when learning different emotions in different environments, or galaxies. The collection is ever evolving. There are 306 unique tokens currently and I have dozens more in the works that haven't been minted yet. So, go <a href="https://opensea.io/collection/states-of-joi" target="_blank" rel="noreferrer">explore the states of Joi</a> and check back often for more!</p>
         <br/>
-        <p>You can also follow the project on <a href="https://twitter.com" target="_blank">Twitter</a> <a href="https://twitter.com/statesofjoi" target="_blank">@statesofjoi</a>.</p>
+        <p>You can also follow the project on <a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a> <a href="https://twitter.com/statesofjoi" target="_blank" rel="noreferrer">@statesofjoi</a>.</p>
       </div>
       <footer>
       <p><sup><small><i>1</i></small></sup><i>This is a super brief explanation of the process I went through. If you're interested in doing something similar I suggest doing your own research; I'm no expert</i>.</p>
-      <p>&copy; <a href="https://github.com/jackfperryjr" target="_blank">@jackfperryjr</a> <a href="https://twitter.com/statesofjoi" target="_blank">@statesofjoi</a></p>
+      <p>&copy; <a href="https://github.com/jackfperryjr" target="_blank" rel="noreferrer">@jackfperryjr</a> <a href="https://twitter.com/statesofjoi" target="_blank" rel="noreferrer">@statesofjoi</a></p>
       </footer>
     </div>
   );
